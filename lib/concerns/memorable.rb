12c20b0f8bc9ac9ce81acaec881b8a275fdbe050
Mickae Truman <mtruman@nyit.edu>
2019-03-24 22:31:48 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,16 @@
 module Memorable
-  
-def reset_all
- all.clear
-end
-
-def count
- all.count
-end 
-
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+ 
+    def count
+      self.all.count
+    end
+  end
+ 
+  module InstanceMethods
+    def initialize
+    end
+  end
 end
