39fb475907a1967a246368e6e491cd6c28c9bd12
Mickae Truman <mtruman@nyit.edu>
2019-03-24 22:41:53 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,22 +1,8 @@
 module Findable
-  
-  class Artist
- 
-  @@artists = []
- 
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  end
- 
-  def self.all
-    @@artists
-  end
  
   def self.find_by_name(name)
     @@artists.detect {|a| a.name == name}
   end
-end
+
 
 end
